881db029ceb7fc2123c1b893422e623b9f1e7640
Marx Bitware <info@marxbitware.co.uk>
2021-10-15 08:47:28 +0700

    Add project files.

---
+++ ZenerCard/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿
+namespace ZenerCard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(732, 47);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(732, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(671, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 31);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "0";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(547, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(101, 123);
+            this.button6.TabIndex = 20;
+            this.button6.Text = "&Start";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(674, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Incorrect:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(682, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Correct:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(671, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "% Correct:";
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
+            this.button5.Location = new System.Drawing.Point(440, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(101, 123);
+            this.button5.TabIndex = 16;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Location = new System.Drawing.Point(333, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(101, 123);
+            this.button4.TabIndex = 15;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Location = new System.Drawing.Point(226, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 123);
+            this.button3.TabIndex = 14;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(119, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 123);
+            this.button2.TabIndex = 13;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 123);
+            this.button1.TabIndex = 12;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(929, 147);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "ZenerCard - ESP Testing Software v1.0.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ZenerCard/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace ZenerCard
+{
+    public partial class Form1 : Form
+    {
+        private int totalAttempts;
+        private int numberCorrect;
+        private int numberIncorrect;
+        private double percentageAccurate;
+        private RNGCryptoServiceProvider rng;
+
+        public enum ZenerCard
+        {
+            Circle,
+            Cross,
+            Wavylines,
+            Square,
+            Star,
+            Null
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            rng = new RNGCryptoServiceProvider();
+        }
+
+        #region Card Operations
+
+        private ZenerCard GetCard()
+        {
+            
+            byte[] r = new byte[1];
+            
+            rng.GetBytes(r);
+
+            if (r[0] < 51)
+                return ZenerCard.Circle;
+            else if (r[0] < 102)
+                return ZenerCard.Cross;
+            else if (r[0] < 153)
+                return ZenerCard.Wavylines;
+            else if (r[0] < 204)
+                return ZenerCard.Square;
+            else if (r[0] < 255)
+                return ZenerCard.Star;
+
+            return ZenerCard.Null;
+        }
+
+        private bool CompareCards(ZenerCard selected, ZenerCard generated)
+        {
+            if (selected == generated)
+                return true;
+            else
+                return false;
+        }
+
+        #endregion
+
+        #region Success/Failure stats update
+
+        private void UpdateSuccessData()
+        {
+            totalAttempts++;
+            numberCorrect++;
+            percentageAccurate = ((double)numberCorrect / (double)totalAttempts) * 100.00;
+            label5.Text = numberCorrect.ToString();
+            label4.Text = percentageAccurate.ToString();
+        }
+
+        private void UpdateFailureData()
+        {
+            totalAttempts++;
+            numberIncorrect++;
+
+            try
+            {
+                percentageAccurate = ((double)numberCorrect / (double)totalAttempts) * 100.00;
+            }
+            catch
+            {
+
+            }
+
+            label6.Text = numberIncorrect.ToString();
+            label4.Text = percentageAccurate.ToString();
+        }
+
+        #endregion
+
+        #region Buttons
+
+        private void button6_Click_1(object sender, EventArgs e)
+        {
+            if (button6.Text.Contains("Start"))
+            {
+                button6.Text = "Reset";
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button4.Enabled = true;
+                button5.Enabled = true;
+            }
+
+            if (button6.Text.Contains("Reset"))
+            {
+                label4.Text = "0";
+                label5.Text = "0";
+                label6.Text = "0";
+
+                totalAttempts = 0;
+                numberCorrect = 0;
+                numberIncorrect = 0;
+                percentageAccurate = 0;
+            }
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            bool result = CompareCards(ZenerCard.Star, GetCard());
+
+            if (result)
+                UpdateSuccessData();
+            else
+                UpdateFailureData();
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            bool result = CompareCards(ZenerCard.Square, GetCard());
+
+            if (result)
+                UpdateSuccessData();
+            else
+                UpdateFailureData();
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            bool result = CompareCards(ZenerCard.Wavylines, GetCard());
+
+            if (result)
+                UpdateSuccessData();
+            else
+                UpdateFailureData();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            bool result = CompareCards(ZenerCard.Cross, GetCard());
+
+            if (result)
+                UpdateSuccessData();
+            else
+                UpdateFailureData();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            bool result = CompareCards(ZenerCard.Circle, GetCard());
+
+            if (result)
+                UpdateSuccessData();
+            else
+                UpdateFailureData();
+        }
+
+        #endregion
+    }
+}
